d43089874ab18a162908aecd180616bb27516a1d
Deepak Joy Jose <deejayjay@zoho.com>
2022-03-29 14:29:17 -0600

    Modifies the styles. Adds the PageTitle

---
+++ components/PageTitle/PageTitle.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+function PageTitle({ children, title, tagline, ...props }) {
+  return (
+    <header className={styles.pageTitle}>
+      <h1>{title}</h1>
+      <p>{tagline}</p>
+    </header>
+  );
+}
+
+export default PageTitle;
